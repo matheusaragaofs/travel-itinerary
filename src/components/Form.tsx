782e81ea677d83166025010f9811cd6b0ff7bc1f
Natan Lima <nfsl@cin.ufpe.br>
2024-07-24 20:18:01 -0300

    auto-complete/date format

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,11 @@
 'use client';
 
 import React, { useState } from 'react';
-import {
-  Col,
-  DatePicker,
-  DatePickerProps,
-  FormProps,
-  InputNumber,
-  InputNumberProps,
-  Row,
-  Select,
-  Slider,
-  Space,
-  Tooltip,
-} from 'antd';
+import { Col, DatePicker, FormProps, InputNumber, InputNumberProps, Row, Select, Slider, Space, Tooltip } from 'antd';
 import { Button, Form, Input } from 'antd';
+import { Dayjs } from 'dayjs';
+import { LoadScript, Autocomplete } from '@react-google-maps/api';
 import '../app/css/form.css';
-import dayjs, { Dayjs } from 'dayjs';
-
-
 
 type FieldType = {
   travel_period: string;
@@ -28,45 +15,38 @@ type FieldType = {
 };
 
 const travelStyleOptions = {
-  'Aventura/Ao ar livre':
-    'Para aqueles interessados em caminhadas, acampamentos, esportes aquáticos e outras atividades aventureiras.',
-  'Cultural/Histórico':
-    'Foco em museus, sítios históricos e experiências culturais.',
+  'Aventura/Ao ar livre': 'Para aqueles interessados em caminhadas, acampamentos, esportes aquáticos e outras atividades aventureiras.',
+  'Cultural/Histórico': 'Foco em museus, sítios históricos e experiências culturais.',
   Luxo: 'Hotéis de alto padrão, gastronomia refinada e experiências exclusivas.',
-  Econômico:
-    'Acomodações acessíveis, atividades econômicas e opções de refeições econômicas.',
-  Familiar:
-    'Atividades adequadas para todas as idades, acomodações adequadas para crianças e atrações.',
-  Romântico:
-    'Escapadas românticas, atividades para casais, experiências especiais para dois.',
-  'Bem-estar/Relaxamento':
-    'Retiro de spa, atividades de bem-estar, sessões de ioga e locais serenos.',
-  'Comida e Bebida':
-    'Tours culinários, degustações de vinhos, experiências de culinária local.',
+  Econômico: 'Acomodações acessíveis, atividades econômicas e opções de refeições econômicas.',
+  Familiar: 'Atividades adequadas para todas as idades, acomodações adequadas para crianças e atrações.',
+  Romântico: 'Escapadas românticas, atividades para casais, experiências especiais para dois.',
+  'Bem-estar/Relaxamento': 'Retiro de spa, atividades de bem-estar, sessões de ioga e locais serenos.',
+  'Comida e Bebida': 'Tours culinários, degustações de vinhos, experiências de culinária local.',
   Compras: 'Foco em distritos de compras, mercados e lojas locais únicas.',
   'Vida Noturna': 'Clubes, bares, música ao vivo e outras atividades noturnas.',
-  'Natureza e Vida Selvagem':
-    'Parques nacionais, safáris de vida selvagem, jardins botânicos.',
-  'Festival/Eventos':
-    'Participação em festivais locais, concertos, eventos esportivos ou outros grandes eventos.',
+  'Natureza e Vida Selvagem': 'Parques nacionais, safáris de vida selvagem, jardins botânicos.',
+  'Festival/Eventos': 'Participação em festivais locais, concertos, eventos esportivos ou outros grandes eventos.',
 };
 
 const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log('Success:', values);
+  const data = {
+    ...values,travel_period: values.travel_period.map((date: Dayjs) => date.format('DD/MM/YYYY')),
+  }
+  console.log('Saída:', data);
 };
 
 const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
   console.log('Failed:', errorInfo);
 };
 
-const onChangeDate = (dates: [Dayjs | null, Dayjs | null] | null, dateStrings: [string, string]) => {
-  
-    console.log('Selected Dates: ', dates);
-    console.log('Formatted Date Strings: ', dateStrings);
-};
+
 
 const ItineraryForm: React.FC = () => {
+  const [form] = Form.useForm();
   const [sliderInputValue, setSliderInputValue] = useState(1);
+  const [autocomplete, setAutocomplete] = useState<google.maps.places.Autocomplete | null>(null);
+  const [address, setAddress] = useState('');
 
   const onChangeSlider: InputNumberProps['onChange'] = (newValue) => {
     setSliderInputValue(newValue as number);
@@ -77,10 +57,25 @@ const ItineraryForm: React.FC = () => {
   };
   const { RangePicker } = DatePicker;
 
+  const onLoad = (autoC: google.maps.places.Autocomplete) => {
+    setAutocomplete(autoC);
+  };
+
+  const onPlaceChanged = () => {
+    if (autocomplete !== null) {
+      const place = autocomplete.getPlace();
+      const addressName = place.name ?? '';
+      setAddress(addressName);
+      form.setFieldsValue({ destination: addressName });
+    } else {
+      console.log('Autocomplete is not loaded yet!');
+    }
+  };
 
   return (
     <div className="form-container">
       <Form
+        form={form}
         name="basic"
         labelCol={{ span: 24 }}
         wrapperCol={{ span: 24 }}
@@ -96,16 +91,39 @@ const ItineraryForm: React.FC = () => {
           rules={[{ required: true, message: 'Por favor digite uma cidade!' }]}
           className="form-item"
         >
-          <Input placeholder="Rio de Janeiro,RJ..." className="custom-input" variant="borderless"/>
+          <LoadScript googleMapsApiKey={'AIzaSyBtVM3Hv__-aJLFy76gC7A2AaQpYKG-1_U'} libraries={['places']}>
+            <Autocomplete
+              fields={['formatted_address', 'geometry', 'name']}
+              onLoad={onLoad}
+              onPlaceChanged={onPlaceChanged}
+            >
+              <Input
+                value={address}
+                onChange={(e) => {
+                  const newValue = e.target.value;
+                  setAddress(newValue);
+                  form.setFieldsValue({ destination: newValue });
+                }}
+                placeholder="Rio de Janeiro, RJ..."
+                className="custom-input"
+                variant="borderless"
+              />
+            </Autocomplete>
+          </LoadScript>
         </Form.Item>
-
+        
         <Form.Item<FieldType>
           label="Período da viagem"
           name="travel_period"
           rules={[{ required: true, message: 'Por favor selecione uma data!' }]}
           className="form-item"
         >
-          <RangePicker variant="borderless" placeholder={['Data Início', 'Data FInal']} format="DD-MM-YYYY"  picker="week" className="custom-input" onChange={onChangeDate} />
+          <RangePicker
+            variant="borderless"
+            placeholder={['Data Início', 'Data Final']}
+            format="DD/MM/YYYY"
+            className="custom-input"
+          />
         </Form.Item>
 
         <Form.Item<FieldType>
@@ -141,21 +159,20 @@ const ItineraryForm: React.FC = () => {
           ]}
           className="form-item"
         >                    
-              <Slider
-                min={1}
-                max={2000}
-                onChange={onChangeSlider}
-                value={
-                  typeof sliderInputValue === 'number' ? sliderInputValue : 0
-                }
-                tooltip={{ open: true, formatter: (value) => `R$${value},00` }} 
-                className="custom-slider"
-              />         
-            
+          <Slider
+            min={1}
+            max={2000}
+            onChange={onChangeSlider}
+            value={
+              typeof sliderInputValue === 'number' ? sliderInputValue : 0
+            }
+            tooltip={{ open: true, formatter: (value) => `R$${value},00` }} 
+            className="custom-slider"
+          />         
         </Form.Item>
 
         <Form.Item className="form-item centered-button" wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit" >
+          <Button type="primary" htmlType="submit">
             Descubra o seu plano
           </Button>
         </Form.Item>
